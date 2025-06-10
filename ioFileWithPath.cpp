19de3ceb6a72a337c88d8c44e5164d9d8edee8b9
Indrianingsihputri <indrianingsihputri30@gmail.com>
2025-06-10 18:16:28 +0700

    menambahkan string dan pertintah nama file

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+    srtring NamaFile;
+    cout << "Masukkan nama file: ";
+    cin >> NamaFile;
+    // membuka file dalam mode menulis
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+    cout << ">= menulis file, \'q\' untuk keluar" << endl;
+
+    while (true) {
+        cout << "- ";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+}
