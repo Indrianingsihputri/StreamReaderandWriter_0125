edb89d55127923b0020e186976fb895da00117ff
Indrianingsihputri <indrianingsihputri30@gmail.com>
2025-06-10 18:49:00 +0700

    membuat perintah awal program

---
+++ exeptionStandard.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <exeption>
+//untuk objek exeption yang akan digunakan 
+#include <array>
+//untuk objek array yang akan digunakan
+using namespace std;
+
+int main()
+{
+    cout << "Awal program" << endl;
+}
