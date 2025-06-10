a791eb01ea0ff2d018c22c01a52689be7f032059
Indrianingsihputri <indrianingsihputri30@gmail.com>
2025-06-10 18:40:54 +0700

    menutup file mode menulis dan membuka file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -22,4 +22,25 @@ int main(){
         // menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+    // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+    // membuka file dalam mode membaca
+    ifstream infile;
+    // menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+    cout << endl << ">= membuka dan membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open()) 
+    {
+        // membaca setiap baris dari file
+        while (getline(infile, baris)) 
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        // tutup file setelah selesai membaca
+        infile.close();
+    } 
+    else cout << "unable to open file" << endl;
+    return 0;
 }
