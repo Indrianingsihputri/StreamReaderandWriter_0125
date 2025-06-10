ef4488535dced87745c65801cace855203226d89
Indrianingsihputri <indrianingsihputri30@gmail.com>
2025-06-10 18:18:24 +0700

    menambahkan file mode tulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -10,17 +10,5 @@ int main(){
     cin >> NamaFile;
     // membuka file dalam mode menulis
     ofstream outfile;
-    // menunjuk ke sebuah nama file
-    outfile.open(NamaFile + ".txt", ios::out);
-    cout << ">= menulis file, \'q\' untuk keluar" << endl;
 
-    while (true) {
-        cout << "- ";
-        // mendapatkan setiap karakter dalam satu baris
-        getline(cin, baris);
-        // loop akan berhenti jika anda memasukkan karakter q
-        if (baris == "q") break;
-        // menulis dan memasukkan nilai dari 'baris' ke dalam file
-        outfile << baris << endl;
-    }
 }
