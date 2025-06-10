4a5b4e7e1a8ed7cb4004f95c9afaac9236205130
Indrianingsihputri <indrianingsihputri30@gmail.com>
2025-06-10 18:51:46 +0700

    menambahkan pesan array 3 elemen dan memanggil array

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -11,5 +11,8 @@ int main()
     try
     {
         array<int, 3> arr = {10, 20, 30,};
+        //pesan array dengan 3 elemen
+        cout << data.at(5) << endl;
+        //memanggil array elemen ke-5 
     }
 }
